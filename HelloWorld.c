e27ebfc1abc2769fbe48157edb03b961ef2d93c4
SKamesiepo <skamesiepo@gmail.com>
2024-05-24 14:49:07 +0200

    Added notes on HelloWorld.c file

---
+++ HelloWorld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("I like pizza!\n");
+    printf("It's really good\n");
+    return 0;
+}
+
+
+
+
+
+
+
+/*Notes For Hello World
+- the top #include is an import statment for input output libraries we would need
+- adding "\n" at the end make the next print statement go to a new line
+*/
