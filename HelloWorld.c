3a8fc67f94d643d2822245790c41cde8d73db850
SKamesiepo <skamesiepo@gmail.com>
2024-05-24 14:53:05 +0200

    Completed basic printout program

--- HelloWorld.c
+++ HelloWorld.c
@@ -16,4 +16,7 @@ int main(){
 /*Notes For Hello World
 - the top #include is an import statment for input output libraries we would need
 - adding "\n" at the end make the next print statement go to a new line
+- the return 0 at the end tests to see if there were any errors with the executed function.
+if no problem you will see how it prints out "exited with code = 0" after it runs in terminal
+else it will show error in the line and exit with code=1
 */
